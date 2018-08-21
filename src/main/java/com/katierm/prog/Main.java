7f82a40f11c5d8b4f3d1b541bdef6ea1273c3520
katierm <miheevakate98@gmail.com>
2018-08-21 16:30:50 +0300

    first commit

---
+++ src/main/java/com/katierm/prog/Main.java
@@ -0,0 +1,7 @@
+package com.katierm.prog;
+
+public class Main {
+    public static void main(String[] args) {
+        com.katier.lib.Main.main(null);
+    }
+}
---
+++ src/test/java/T.java
@@ -0,0 +1,11 @@
+import com.katierm.prog.Main;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class T {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(com.katier.lib.Main.f()==42);
+    }
+}
